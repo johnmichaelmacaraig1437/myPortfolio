38862b45dde4786d6bd20ae91ef77087f9b8711c
John Michael Macaraig <johnmichael.macaraig@brother-biph.com.ph>
2026-03-31 08:13:28 +0800

    fix: install vite for vercel build

--- MyPortfolio.Server/obj/Debug/net9.0/MyPortfolio.Server.AssemblyInfo.cs
+++ MyPortfolio.Server/obj/Debug/net9.0/MyPortfolio.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPortfolio.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c36c7ab63cd4f3d490353609a2ad8771d8da723")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyPortfolio.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPortfolio.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
